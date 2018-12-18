6300cd5620ab32f4578ee2e908515815aa9614ea
cook1ee <43473525+cook1ee@users.noreply.github.com>
2018-12-18 10:21:33 +0100

    better chat validation, dark mode

---
+++ chat.js
@@ -0,0 +1,55 @@
+function sendChatMessage(message)
+{
+	message = validateChatMessage(message);
+	
+	if(!(/^( )*$/g.test(message))) //dont send empty messages
+	{
+		database.ref().child("chatmessages").child(Date.now()).set({
+			sender: username,
+			time: Date.now(),
+			message: message
+		});
+	}
+	
+	nodes.input.chatMessage.value = "";
+}
+
+function addChatMessage(messageObject) //add to messages div
+{
+	let messageDiv = nodes.container.chatboxMessages.appendChild(document.createElement("div"));
+	messageDiv.classList.add("chatmessage");
+	messageDiv.innerHTML = "<span class='name'>" + messageObject.sender + "</span><br/>" +
+							messageObject.message + "<br/>" +
+							"<span  class='time'>" + TimeFormatter.format(messageObject.time) + "<span>";
+	nodes.container.chatboxMessages.scrollTop += nodes.container.chatboxMessages.clientHeight;
+}
+
+function validateChatMessage(message)
+{
+	message = message.match(/[a-z|A-Z|0-9|?!.,:; ]*/g).join("");
+	if(message.length > 100)
+	{
+		message = message.substring(0, 100);
+	}
+	return message;
+}
+
+nodes.button.sendChatMessage.onclick = function(e)
+{
+	sendChatMessage(nodes.input.chatMessage.value);
+}
+
+nodes.input.chatMessage.oninput = function(e)
+{
+	nodes.input.chatMessage.value = validateChatMessage(nodes.input.chatMessage.value);
+}
+
+nodes.container.chatbox.onmouseover = function(e)
+{
+	cursorOnContainer = true;
+}
+
+nodes.container.chatbox.onmouseout = function(e)
+{
+	cursorOnContainer = false;
+}
---
+++ preload.js
@@ -0,0 +1,84 @@
+var nodes =
+{
+	toolbar_color_preview: document.getElementById("toolbar_color_preview"),
+	container:
+	{
+		toolbar: document.getElementById("toolbar_container"),
+		chatbox: document.getElementById("chatbox_container"),
+		chatboxMessages: document.getElementById("chatbox_messages")
+	},
+	button:
+	{
+		close_toolbar: document.getElementById("button_close_toolbar"),
+		toggleDarkMode: document.getElementById("button_toggle_darkmode"),
+		tools:
+		{
+			ellipse: document.getElementById("button_tool_ellipse"),
+			rectangle: document.getElementById("button_tool_rectangle"),
+			line: document.getElementById("button_tool_line"),
+			text: document.getElementById("button_tool_text"),
+			image: document.getElementById("button_tool_image"),
+			view: document.getElementById("button_tool_view")
+		},
+		sendChatMessage: document.getElementById("chatbox_button_send")
+	},
+	slider:
+	{
+		color:
+		{
+			hue: document.getElementById("toolbar_slider_hue"),
+			saturation: document.getElementById("toolbar_slider_saturation"),
+			luminance: document.getElementById("toolbar_slider_luminance")
+		},
+		brushSize:
+		{
+			x: document.getElementById("toolbar_slider_brushSize_x"),
+			y: document.getElementById("toolbar_slider_brushSize_y"),
+			radius: document.getElementById("toolbar_slider_brushSize_radius")
+		},
+		image:
+		{
+			rotation: document.getElementById("toolbar_slider_image_rotation"),
+			index: document.getElementById("toolbar_slider_image_index")
+		}
+	},
+	input:
+	{
+		chatMessage: document.getElementById("chatbox_input_message")
+	},
+	text:
+	{
+		usersOnline: document.getElementById("text_users_online")
+	},
+	checkbox:
+	{
+		allowTint: document.getElementById("toolbar_check_allowtint")
+	}
+};
+
+var settings =
+{
+	darkMode: false,
+	toolbarHidden: false,
+	color: "hsl(0, 0%, 0%)",
+	sliderValues:
+	{
+		color:
+		{
+			hue: 0,
+			saturation: 0,
+			luminance: 0
+		}
+	},
+	tool: "ellipse",
+	brushSize:
+	{
+		x: "0.1", y: "0.1", radius: "0.05"
+	},
+	image:
+	{
+		index: 0,
+		rotation: 0,
+		allowTint: false
+	}
+};
