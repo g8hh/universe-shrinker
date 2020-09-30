b69c5462d3d6ad6030e97e7a53313c6eb22431ba
cook1ee <kai.bachor@gmail.com>
2020-09-30 19:35:25 +0200

    challenge fix

--- omega-layers/js/classes/prestigelayer.js
+++ omega-layers/js/classes/prestigelayer.js
@@ -143,7 +143,7 @@ class PrestigeLayer
                 case UPGRADE_GENMULTI:
                     upg = new Upgrade(this, game.layers[0] || this,
                         level => Utils.createValueDilation(Decimal.pow(3 * i + 2 + extraPriceIncrease, level).mul(bp), 0.01).pow(Upgrade.getPricePower()),
-                        level => new Decimal(effectGenMulti * level).pow(Upgrade.getEffectPower()), UPGRADE_GENMULTI, {
+                        level => new Decimal(effectGenMulti * level).pow(new Decimal(1).div(Upgrade.getEffectPower())), UPGRADE_GENMULTI, {
                             getEffectDisplay: effectDisplayTemplates.numberStandard(3, "+")
                         });
                     break;
